31c4d067f1b4e23527cd01348c7b57cfac276c3f
Erin Fitzpatric <efitzpatric@gmail.com>
2021-03-07 01:41:22 -0500

    Added Vue and integrated with Electron

---
+++ electron/castmate-frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="G'Day CastMate!!!"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
