9cb97ef4a52f912dbfaeadf35fa43d8ce6b5b3f2
Ben Decker <deckerbd@users.noreply.github.com>
2021-03-13 00:47:18 -0500

    Got source maps working

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+	configureWebpack: {
+		devtool: 'source-map'
+	},
 	pluginOptions: {
 		electronBuilder: {
 			externals: ["win32-api","ffi-napi", "ref-napi", "node-gyp-build", "@peter-murray/hue-bridge-model", "node-hue-api"],
