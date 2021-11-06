a91d6e192f73b141b603e781a16a353e9a32f611
LordTocs <Tocs1001@gmail.com>
2021-11-06 19:18:56 -0400

    Set 'ws' module as external to fix packaging issues

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
 	pluginOptions: {
 		electronBuilder: {
-			externals: ["win32-api", "ffi-napi", "ref-napi", "node-gyp-build", "@peter-murray/hue-bridge-model", "node-hue-api", "jsdom", "canvas", "chokidar", 'twitch', 'twitch-chat-client', 'twitch-pubsub-client', 'twitch-webhooks'],
+			externals: ["win32-api", "ffi-napi", "ref-napi", "node-gyp-build", "@peter-murray/hue-bridge-model", "node-hue-api", "jsdom", "canvas", "chokidar", 'twitch', 'twitch-chat-client', 'twitch-pubsub-client', 'twitch-webhooks', 'ws'],
 			nodeIntegration: true,
 			builderOptions: {
 				nsis: {
