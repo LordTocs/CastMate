31f3b00d9035c01dd2b63ebc4db9d3edfdfacdd5
LordTocs <Tocs1001@gmail.com>
2021-12-01 22:17:20 -0500

    Add reset button and fix booleans

--- src/components/data/DataInput.vue
+++ src/components/data/DataInput.vue
@@ -1,67 +1,70 @@
 <template>
-  <number-input
-    :value="value"
-    @input="(v) => $emit('input', v)"
-    v-if="schema.type == 'Number' && !schema.slider"
-    :allowTemplate="!!schema.template"
-    :label="schema.name || label"
-  />
-  <v-slider
-    v-else-if="schema.type == 'Number' && schema.slider"
-    :label="schema.name || label"
-    :value="value"
-    :min="schema.slider.min"
-    :max="schema.slider.max"
-    :step="schema.slider.step"
-    color="white"
-    @input="(v) => $emit('input', v)"
-  />
-  <string-data-input
-    :value="value"
-    @input="(v) => $emit('input', v)"
-    v-else-if="schema.type == 'String'"
-    :dataName="schema.name || label"
-    :schema="schema"
-  />
-  <v-switch
-    :value="value"
-    @change="(v) => $emit('input', v)"
-    v-else-if="schema.type == 'Boolean'"
-    :label="schema.name || label"
-  />
-  <v-select
-    :value="value"
-    :label="schema.name || label"
-    @change="(v) => $emit('input', v)"
-    :items="[
-      { name: 'On', value: true },
-      { name: 'Off', value: false },
-      { name: 'unset', value: undefined },
-    ]"
-    item-text="name"
-    item-value="value"
-    v-else-if="schema.type == 'OptionalBoolean'"
-  />
-  <file-autocomplete
-    v-else-if="schema.type == 'FilePath'"
-    :value="value"
-    @change="(v) => $emit('input', v)"
-    :recursive="!!schema.recursive"
-    :path="schema.path"
-    :basePath="schema.basePath"
-  />
-  <color-picker
-    :value="value"
-    @input="(v) => $emit('input', v)"
-    v-else-if="schema.type == 'LightColor'"
-    :schema="schema"
-  />
   <object-editor
     :schema="schema.properties"
     :value="value"
     @input="(v) => $emit('input', v)"
-    v-else-if="schema.type == 'Object' && schema.properties"
+    v-if="schema.type == 'Object' && schema.properties"
   />
+  <div class="input-row" v-else>
+    <number-input
+      :value="value"
+      @input="(v) => $emit('input', v)"
+      v-if="schema.type == 'Number' && !schema.slider"
+      :allowTemplate="!!schema.template"
+      :label="schema.name || label"
+    />
+    <v-slider
+      v-else-if="schema.type == 'Number' && schema.slider"
+      :label="schema.name || label"
+      :value="value"
+      :min="schema.slider.min"
+      :max="schema.slider.max"
+      :step="schema.slider.step"
+      color="white"
+      @input="(v) => $emit('input', v)"
+    />
+    <string-data-input
+      :value="value"
+      @input="(v) => $emit('input', v)"
+      v-else-if="schema.type == 'String'"
+      :dataName="schema.name || label"
+      :schema="schema"
+    />
+    <v-switch
+      :input-value="value"
+      @change="(v) => $emit('input', v)"
+      v-else-if="schema.type == 'Boolean'"
+      :label="schema.name || label"
+    />
+    <v-select
+      :value="value"
+      :label="schema.name || label"
+      @change="(v) => $emit('input', v)"
+      :items="[
+        { name: 'On', value: true },
+        { name: 'Off', value: false },
+        { name: 'unset', value: undefined },
+      ]"
+      item-text="name"
+      item-value="value"
+      v-else-if="schema.type == 'OptionalBoolean'"
+    />
+    <file-autocomplete
+      v-else-if="schema.type == 'FilePath'"
+      :value="value"
+      @change="(v) => $emit('input', v)"
+      :recursive="!!schema.recursive"
+      :path="schema.path"
+      :basePath="schema.basePath"
+    />
+    <color-picker
+      :value="value"
+      @input="(v) => $emit('input', v)"
+      v-else-if="schema.type == 'LightColor'"
+      :schema="schema"
+    />
+    <v-btn v-if="!schema.required" @click="(v) => $emit('input', undefined)"> Reset </v-btn>
+  </div>
 </template>
 
 <script>
@@ -89,21 +92,8 @@ export default {
 </script>
 
 <style>
-.object-label {
-  font-size: 14px;
-  color: #606266;
-  line-height: 32px;
-  display: inline-block;
-  text-align: right;
-  padding-right: 0.5rem;
-}
-
 .input-row {
   display: flex;
   flex-direction: row;
-}
-
-.input-row:not(:last-child) {
-  margin-bottom: 0.35rem;
 }
 </style>
