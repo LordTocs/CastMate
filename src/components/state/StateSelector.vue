3f0ded7ca8c2da8d6c0f6b2a0a03e53e3b7923a2
LordTocs <Tocs1001@gmail.com>
2021-12-14 01:12:23 -0500

    Turn the state selector into a combobox and show the plugin and current value in the selector

--- src/components/state/StateSelector.vue
+++ src/components/state/StateSelector.vue
@@ -1,10 +1,45 @@
 <template>
-  <v-select
+  <v-combobox
+    :items="stateList"
     :value="value"
+    item-text="key"
     @change="(v) => $emit('input', v)"
-    :label="label"
-    :items="stateNames"
-  />
+  >
+    <template v-slot:item="{ item }">
+      <v-chip
+        class="mr-2"
+        v-if="item"
+        :color="getPlugin(item.plugin).color"
+        outlined
+        small
+      >
+        {{ getPlugin(item.plugin).uiName }}
+      </v-chip>
+      <span v-if="item">
+        {{ item.key }}
+      </span>
+      <span v-if="item" class="text--secondary ml-2">
+        ({{ stateLookup[item.plugin][item.key] }})
+      </span>
+    </template>
+    <template v-slot:selection="{ item }">
+      <v-chip
+        class="mr-2"
+        v-if="item"
+        :color="getPlugin(item.plugin).color"
+        outlined
+        small
+      >
+        {{ getPlugin(item.plugin).uiName }}
+      </v-chip>
+      <span v-if="item">
+        {{ item.key }}
+      </span>
+      <span v-if="item" class="text--secondary ml-2">
+        ({{ stateLookup[item.plugin][item.key] }})
+      </span>
+    </template>
+  </v-combobox>
 </template>
 
 <script>
@@ -14,18 +49,22 @@ export default {
     value: {},
     label: {},
   },
-  data() {
-    return {
-      stateNames: [],
-    };
-  },
   computed: {
-    ...mapGetters("ipc", ["client"]),
+    ...mapGetters("ipc", ["stateLookup", "plugins"]),
+    stateList() {
+      const stateList = [];
+      for (let pluginName of Object.keys(this.stateLookup)) {
+        for (let stateKey of Object.keys(this.stateLookup[pluginName])) {
+          stateList.push({ plugin: pluginName, key: stateKey });
+        }
+      }
+      return stateList;
+    },
   },
-  async mounted() {
-    let state = await this.client.getCombinedState();
-
-    this.stateNames = Object.keys(state);
+  methods: {
+    getPlugin(pluginName) {
+      return this.plugins.find((p) => p.name == pluginName);
+    },
   },
 };
 </script>
