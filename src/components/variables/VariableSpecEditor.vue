6cb6892e5911946844882db347deeb82a9c543c8
LordTocs <Tocs1001@gmail.com>
2022-01-08 01:45:25 -0500

    Make the variable editer respect types for default values

--- src/components/variables/VariableSpecEditor.vue
+++ src/components/variables/VariableSpecEditor.vue
@@ -20,13 +20,22 @@
       :value="value.default"
       @change="(v) => updateSubValue('default', v)"
       label="Default Value"
+      v-if="value.type == 'String'"
+    />
+    <number-input
+      v-else
+      :value="value.default"
+      @input="(v) => updateSubValue('default', v)"
+      label="Default Value"
     />
   </div>
 </template>
 
 <script>
 import _cloneDeep from "lodash/cloneDeep";
+import NumberInput from "../data/NumberInput.vue";
 export default {
+  components: { NumberInput },
   props: {
     variableName: { type: String },
     value: {},
