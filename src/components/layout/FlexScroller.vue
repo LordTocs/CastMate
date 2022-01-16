3e7e650cf346c46d9939eb5caea0e99f34193aa2
LordTocs <Tocs1001@gmail.com>
2022-01-15 23:18:28 -0500

    Place headers with save button outside of the scrollable area so it's always accessible

---
+++ src/components/layout/FlexScroller.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="scroller-outer">
+    <div class="scroller-inner">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.scroller-outer {
+  position: relative;
+}
+.scroller-inner {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+}
+</style>
