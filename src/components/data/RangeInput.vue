6ee699bacd18ccbf9c5c12478ab8fc1486f8419a
LordTocs <Tocs1001@gmail.com>
2022-05-13 18:53:05 -0400

    Improve range inputs to share a label and use infinity placeholders

--- src/components/data/RangeInput.vue
+++ src/components/data/RangeInput.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="d-flex flex-row align-center">
-    <number-input
-      :label="`Min ${label}`"
-      :value="value ? value.min : null"
-      @input="(v) => setValue(v, 'min')"
-    />
-    <span class="mx-3">⟶</span>
-    <number-input
-      :label="`Max ${label}`"
-      :value="value ? value.max : null"
-      @input="(v) => setValue(v, 'max')"
-    />
+  <div>
+    <span class="label-span">
+      {{ label }}
+    </span>
+    <div class="d-flex flex-row align-center">
+      <number-input
+        class="my-0 py-0"
+        :value="value ? value.min : null"
+        @input="(v) => setValue(v, 'min')"
+        placeholder="−∞"
+      />
+      <span class="mx-3">⟶</span>
+      <number-input
+        class="my-0 py-0"
+        :value="value ? value.max : null"
+        @input="(v) => setValue(v, 'max')"
+        placeholder="∞"
+      />
+    </div>
   </div>
 </template>
 
@@ -33,4 +40,12 @@ export default {
 </script>
 
 <style scoped>
+.label-span {
+  display: inline-block;
+  position: relative;
+  line-height: 15px;
+  font-size: 12px;
+  min-height: 6px;
+  bottom: -6px;
+}
 </style>
