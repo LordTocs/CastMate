f2567fd561658f8654c108ca1f910fc2c17235be
LordTocs <Tocs1001@gmail.com>
2022-05-13 18:55:56 -0400

    Improve Trigger Edit Modal's config area

--- src/components/triggers/TriggerEditModal.vue
+++ src/components/triggers/TriggerEditModal.vue
@@ -2,31 +2,55 @@
   <v-dialog v-model="dialog" width="85%" persistent>
     <v-card>
       <v-toolbar dense flat>
-        <v-toolbar-title class="text-body-2 font-weight-bold grey--text">
+        <v-toolbar-title class="font-weight-bold grey--text">
           {{ header }}
         </v-toolbar-title>
         <trigger-selector
           :value="localTriggerType"
           @input="changeTriggerType"
           label=""
+          class="mx-4"
+          style="width: 150px !important"
         />
       </v-toolbar>
       <div class="d-flex flex-row">
-        <flex-scroller style="width: 300px" color="grey darken-4" innerClass="px-4">
-          <data-input
-            v-if="
-              localTriggerType &&
-              plugins[localTriggerType.pluginKey].triggers[
-                localTriggerType.triggerKey
-              ].config
-            "
-            :schema="
-              plugins[localTriggerType.pluginKey].triggers[
-                localTriggerType.triggerKey
-              ].config
-            "
-            v-model="localMapping.config"
-          />
+        <flex-scroller
+          style="width: 300px"
+          color="grey darken-4"
+          innerClass="px-2"
+        >
+          <v-sheet outlined rounded class="px-2 py-2 my-2">
+            <p class="my-2">
+              {{
+                plugins[localTriggerType.pluginKey].triggers[
+                  localTriggerType.triggerKey
+                ].name
+              }}
+            </p>
+            <p class="text--secondary my-1">
+              {{
+                plugins[localTriggerType.pluginKey].triggers[
+                  localTriggerType.triggerKey
+                ].description
+              }}
+            </p>
+            <v-divider />
+            <data-input
+              v-if="
+                localTriggerType &&
+                plugins[localTriggerType.pluginKey].triggers[
+                  localTriggerType.triggerKey
+                ].config
+              "
+              :schema="
+                plugins[localTriggerType.pluginKey].triggers[
+                  localTriggerType.triggerKey
+                ].config
+              "
+              v-model="localMapping.config"
+            />
+            <p v-else class="text--centered py-4">No Configuration Needed</p>
+          </v-sheet>
         </flex-scroller>
         <automation-full-input class="flex-grow-1" />
       </div>
