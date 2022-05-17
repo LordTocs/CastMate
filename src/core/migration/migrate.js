b0470c4a636b15e7aeec33f5c7cd84ab2822dc4c
LordTocs <Tocs1001@gmail.com>
2022-05-17 17:22:15 -0400

    Prevent new triggerkey overlap stomping

--- src/core/migration/migrate.js
+++ src/core/migration/migrate.js
@@ -55,7 +55,9 @@ function migrateProfile1_0to2_0(profile, changeTracker) {
                 newTriggerKey = 'chat';
             }
 
-            newProfile.triggers[pluginKey][newTriggerKey] = [];
+            if (!newProfile.triggers[pluginKey][newTriggerKey]) {
+                newProfile.triggers[pluginKey][newTriggerKey] = [];
+            }
 
             if (triggerKey != "follow" && triggerKey != 'firstTimeChat') {
                 for (let commandKey in profile.triggers[pluginKey][triggerKey]) {
