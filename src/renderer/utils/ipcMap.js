3b56a79c2fb3200847e729fe218d14c5e778c85a
LordTocs <Tocs1001@gmail.com>
2022-07-30 19:21:05 -0400

    ipcRenderer doesn't understand proxies, so we use lodash clonedeep to pre-clone the object before sending

--- src/renderer/utils/ipcMap.js
+++ src/renderer/utils/ipcMap.js
@@ -1,4 +1,5 @@
 import { ipcRenderer } from 'electron'; 
+import _cloneDeep from 'lodash/cloneDeep'
 
 export function mapIpcs(pluginName, functions)
 {
@@ -7,7 +8,8 @@ export function mapIpcs(pluginName, functions)
 	{
 		console.log(`Binding ${ipcMethod}`)
 		result[ipcMethod] = async function (...args) {
-			return await ipcRenderer.invoke(`${pluginName}_${ipcMethod}`, ...args);
+			//Do we really have to clone? It appears ipcRenderer.invoke does not understand what a Proxy is.
+			return await ipcRenderer.invoke(`${pluginName}_${ipcMethod}`, ...(args.map(a => _cloneDeep(a))));
 		}
 	}
 	return result;
