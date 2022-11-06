96c94ab2a14b9a578df9df596145dd7e0256029b
LordTocs <Tocs1001@gmail.com>
2022-11-06 00:33:42 -0400

    Update obs overlay widget loader to properly use asyncComponents and set positional style

--- packages/castmate-obs-overlay/src/loader/WidgetLoader.vue
+++ packages/castmate-obs-overlay/src/loader/WidgetLoader.vue
@@ -1,9 +1,10 @@
 <template>
-    <component :is="dynamicComponent" v-bind="widgetConfig.props ? widgetConfig.props : {}"> </component>
+    <component :is="dynamicComponent" v-bind="widgetConfig.props ? widgetConfig.props : {}" class="widget" :style="widgetStyle" > </component>
 </template>
 
 <script>
 import loadWidget from 'castmate-overlay-components';
+import { defineAsyncComponent } from 'vue';
 
 export default {
     props: {
@@ -11,8 +12,29 @@ export default {
     },
     computed: {
         dynamicComponent() {
-            return loadWidget(this.widgetConfig.type)
+            const component = defineAsyncComponent({
+                loader: async () => {
+                    const widget = await loadWidget(this.widgetConfig.type)
+                    return widget
+                }
+            })
+            
+            return component
+        },
+        widgetStyle() {
+            return {
+                left: `${this.widgetConfig?.position.x}px`,
+                top: `${this.widgetConfig?.position.y}px`,
+                width: `${this.widgetConfig?.size.width}px`,
+                height: `${this.widgetConfig?.size.height}px`,
+            }
         }
     }
 }
 </script>
+
+<style scoped>
+.widget {
+    position: absolute;
+}
+</style>
