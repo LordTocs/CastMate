6006a614d53382960fcf2ce56ea22ffa16ef89bb
LordTocs <Tocs1001@gmail.com>
2023-02-13 17:30:56 -0500

    Improve Array Inputs with draggable and duplication

--- packages/castmate/src/renderer/components/data/types/ArrayInput.vue
+++ packages/castmate/src/renderer/components/data/types/ArrayInput.vue
@@ -2,26 +2,33 @@
     <v-card variant="outlined" class="my-2">
         <v-card-subtitle class="d-flex flex-row py-2 align-center">
             <div class="flex-grow-1 "> {{ label || 'No label' }} </div> 
-            <v-btn icon="mdi-plus" @click="addItem" size="x-small" />
+            <v-btn icon="mdi-plus" @click="addItem" size="x-small" elevation="0"/>
         </v-card-subtitle>
         <v-divider />
         <div class="my-2">
-            <template v-for="item,i in (modelValue || [])">
-                <div class="d-flex flex-row mx-2 align-center" >
-                    <div class="flex-grow-1">
-                        <data-input 
-                            :schema="itemSchema" 
-                            :model-value="item" 
-                            @update:model-value="(v) => updateItem(i, v)"
-                            :density="density"
-                            :secret="secret"
-                            :context="context"
-                        />
-                    </div>
-                    <v-btn class="mx-2" icon="mdi-delete" @click="deleteItem(i)" size="x-small"/>
-                </div>
-                <v-divider class="my-2"/>
-            </template>
+            <draggable v-model="modelObj" handle=".array-handle">
+                <template #item="{element, index}">
+                    <v-card class="d-flex flex-row mx-2 my-2 align-stretch" elevation="2">
+                        <div class="array-handle">
+                            <v-icon size="large" class="mx-2"> mdi-drag </v-icon>
+                        </div>
+                        <div class="flex-grow-1">
+                            <data-input 
+                                :schema="itemSchema" 
+                                :model-value="element" 
+                                @update:model-value="(v) => updateItem(index, v)"
+                                :density="density"
+                                :secret="secret"
+                                :context="context"
+                            />
+                        </div>
+                        <div class="d-flex flex-column align-center">
+                            <v-btn class="mx-2" icon="mdi-delete" @click="deleteItem(index)" size="x-small" elevation="0"/>
+                            <v-btn class="mx-2" icon="mdi-content-copy" @click="duplicateItem(index)" size="x-small" elevation="0"/>
+                        </div>
+                    </v-card>
+                </template>
+            </draggable>
         </div>
     </v-card>
 </template>
@@ -29,6 +36,9 @@
 <script>
 import { constructDefaultSchema } from '../../../utils/objects';
 import { defineAsyncComponent } from 'vue';
+import Draggable from "vuedraggable";
+import { mapModel } from "../../../utils/modelValue";
+import _cloneDeep from "lodash/cloneDeep"
 export default {
     name: 'array-input',
     props: {
@@ -40,8 +50,9 @@ export default {
         context: { type: {} },
     },
     emits: ['update:modelValue'],
-    components: { DataInput: defineAsyncComponent(() => import("../DataInput.vue")) },
+    components: { DataInput: defineAsyncComponent(() => import("../DataInput.vue")), Draggable },
     computed: {
+        ...mapModel(),
         itemSchema() {
             return this.schema?.items;
         }
@@ -57,6 +68,13 @@ export default {
             newValue.splice(index, 1);
             this.$emit("update:modelValue", newValue);
         },
+        duplicateItem(index) {
+            const newValue = [...(this.modelValue || [])];
+            console.log("Duplicating", index, newValue[index])
+            const newItem = _cloneDeep(newValue[index]);
+            newValue.splice(index, 0, newItem)
+            this.$emit("update:modelValue", newValue);
+        },
         addItem() {
             const newItem = constructDefaultSchema(this.itemSchema);
             const newValue = [...(this.modelValue || []), newItem];
@@ -67,4 +85,11 @@ export default {
 </script>
 
 <style scoped>
+.array-handle {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    cursor: grab;
+}
 </style>
