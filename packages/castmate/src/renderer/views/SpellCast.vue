2de51cb28374f4bc1d301a1d369e6a061afeeebd
LordTocs <Tocs1001@gmail.com>
2023-02-27 12:39:50 -0500

    Add Add and Delete buttons to SpellCast

--- packages/castmate/src/renderer/views/SpellCast.vue
+++ packages/castmate/src/renderer/views/SpellCast.vue
@@ -2,7 +2,7 @@
 	<v-container fluid>
 		<v-card v-if="extensionActive">
 			<div
-				class="d-flex flex-row align-center px-2 py-2 text-h4"
+				class="d-flex flex-row justify-center align-center px-2 py-2 text-h4"
 				style="
 					background: linear-gradient(
 						159deg,
@@ -13,7 +13,18 @@
 					padding-top: 1rem !important;
 				"
 			>
-				<spell-cast-logo />
+				<a href="https://www.spellcast.gg" target="_blank">
+					<spell-cast-logo />
+				</a>
+				<v-text-field
+					class="mx-3"
+					v-model="filter"
+					label="Filter"
+					variant="outlined"
+					prepend-inner-icon="mdi-magnify"
+					clearable
+					hide-details
+				></v-text-field>
 			</div>
 			<v-table>
 				<thead>
@@ -21,11 +32,15 @@
 						<th>Enabled</th>
 						<th>Spell</th>
 						<th></th>
-						<th></th>
+						<th>
+							<v-btn color="#0373e6" variant="flat" @click="dlg.showCreate()">
+								Create Spell
+							</v-btn>
+						</th>
 					</tr>
 				</thead>
 				<tbody v-if="spells.length > 0">
-					<tr v-for="(spell, i) in spells" :key="spell.id">
+					<tr v-for="(spell, i) in filteredSpells" :key="spell.id">
 						<td class="fitwidth">
 							<v-switch
 								color="primary"
@@ -52,13 +67,33 @@
 								variant="flat"
 								@click="dlg.showEdit(spell.id)"
 							/>
+							<v-btn
+								size="small"
+								icon="mdi-delete"
+								class="mx-1"
+								variant="flat"
+								@click="tryDelete(spell.id)"
+							/>
 						</td>
 					</tr>
 				</tbody>
+                <tfoot>
+					<tr>
+						<th></th>
+						<th></th>
+						<th></th>
+						<th>
+							<v-btn color="#0373e6" variant="flat" @click="dlg.showCreate()">
+								Create Spell
+							</v-btn>
+						</th>
+					</tr>
+				</tfoot>
 			</v-table>
 		</v-card>
 		<spell-cast-extension-card v-else />
 		<resource-dialog resourceTypeId="spell" ref="dlg" />
+		<confirm-dialog ref="deleteDlg" />
 	</v-container>
 </template>
 
@@ -70,6 +105,7 @@ import ResourceDialog from "../components/dialogs/ResourceDialog.vue"
 import SpellCastButtonPreview from "../components/spellcast/SpellCastButtonPreview.vue"
 import SpellCastExtensionCard from "../components/spellcast/SpellCastExtensionCard.vue"
 import SpellCastLogo from "../components/spellcast/SpellCastLogo.vue"
+import ConfirmDialog from "../components/dialogs/ConfirmDialog.vue"
 
 const pluginStore = usePluginStore()
 const extensionActive = computed(
@@ -84,6 +120,34 @@ async function toggleSpell(spellId, enabled) {
 }
 
 const dlg = ref(null)
+const deleteDlg = ref(null)
+
+const filter = ref("")
+
+const filteredSpells = computed(() => {
+	if (!filter || filter.length == 0) {
+		return spells.value
+	}
+
+	return spells.value.filter((s) =>
+		s.config.name.toLowerCase().includes(filter.value.toLowerCase())
+	)
+})
+
+async function tryDelete(spellId) {
+	const spell = spells.value.find((s) => s.id == spellId)
+
+	if (!spell) return
+
+	if (
+		await deleteDlg.value.open(
+			"Confirm",
+			`Are you sure you want to delete ${spell.config.name}`
+		)
+	) {
+		await spellFuncs.delete(spellId)
+	}
+}
 </script>
 
 <style scoped>
