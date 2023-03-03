a3ee2d3c983c0b0977468303179638e3e67abfeb
LordTocs <Tocs1001@gmail.com>
2023-03-03 13:36:10 -0500

    Add Missing Builtin Plugin!

--- packages/castmate/src/renderer/store/plugins.js
+++ packages/castmate/src/renderer/store/plugins.js
@@ -3,6 +3,50 @@ import { defineStore } from "pinia"
 import { computed, ref } from "vue"
 import { useIpc } from "../utils/ipcMap"
 
+
+
+const builtInPlugin = {
+	name: 'castmate',
+	uiName: 'CastMate',
+	color: '#8DC1C0',
+	icon: 'mdi-alpha-c-box',
+	actions: {
+		delay: {
+			name: "Delay",
+			color: '#8DC1C0',
+			icon: "mdi-timer-sand",
+			data: { type: "Number", required: true, unit: { name: "Seconds", short: "s" }, },
+			description: "Puts a delay after the current action",
+		},
+		timestamp: {
+			name: "Timestamp",
+			color: '#8DC1C0',
+			icon: "mdi-clock-outline",
+			data: { type: "Number", required: true, unit: { name: "Seconds", short: "s" }, },
+			description: "Delays execution of this action until a certain time after the start of this action list."
+		},
+		automation: {
+			name: "Automation",
+			color: '#8DC1C0',
+			icon: "mdi-cog",
+			data: {
+				type: "Object",
+				properties: {
+					automation: { type: "Automation", required: true },
+				},
+			},
+			description: "Runs another automation inside this one."
+		},
+	},
+	settings: {
+		port: { type: "Number", default: 80, name: "Internal Webserver Port" }
+	},
+	secrets: {},
+	triggers: {},
+	stateSchemas: {},
+	ipcMethods: [],
+}
+
 export const usePluginStore = defineStore("plugins", () => {
 	const plugins = ref({})
 
@@ -13,6 +57,7 @@ export const usePluginStore = defineStore("plugins", () => {
 
 	async function init() {
 		plugins.value = await getPlugins()
+		plugins.value['castmate'] = builtInPlugin
 
 		rootState.value = await getRootState()
 
