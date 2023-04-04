a2cca65d8b08c5e63b399404bc185f672dc68431
LordTocs <Tocs1001@gmail.com>
2023-04-04 01:28:04 -0400

    Make Service Mixin

--- libs/castmate-core/src/util/service.ts
+++ libs/castmate-core/src/util/service.ts
@@ -1,3 +1,21 @@
+export function Service<ConstructorType extends new (...args: any[]) => any>(
+	constructor: ConstructorType
+) {
+	return class Service extends constructor {
+		private static _instance: InstanceType<ConstructorType>
 
+		static initialize(): InstanceType<ConstructorType> {
+			console.log("Initializing Service")
+			if (this._instance) {
+				throw new Error("Service already inited")
+			}
 
+			this._instance = new constructor()
+			return this._instance
+		}
 
+		static getInstance(): InstanceType<ConstructorType> {
+			return this._instance
+		}
+	}
+}
