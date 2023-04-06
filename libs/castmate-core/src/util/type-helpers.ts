e2ca1337aa1e41f1face15c822461d4ea87dd697
LordTocs <Tocs1001@gmail.com>
2023-04-06 02:56:52 -0400

    Add SemanticVersion type

--- libs/castmate-core/src/util/type-helpers.ts
+++ libs/castmate-core/src/util/type-helpers.ts
@@ -7,3 +7,8 @@ export const isArray = Array.isArray
 export function isObject(value: unknown): value is object {
 	return typeof value === "object"
 }
+
+type SV3 = `${number}.${number}.${number}`
+type SV3Label = `${number}.${number}.${number}${string}`
+
+export type SemanticVersion = SV3 | SV3Label
