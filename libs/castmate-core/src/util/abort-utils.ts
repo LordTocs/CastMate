96ea48473a176c2c80be9ac336e367465d99208a
LordTocs <Tocs1001@gmail.com>
2023-04-06 02:52:40 -0400

    Add utility for a timeout that is abortable

---
+++ libs/castmate-core/src/util/abort-utils.ts
@@ -0,0 +1,21 @@
+//https://www.bennadel.com/blog/4195-using-abortcontroller-to-debounce-settimeout-calls-in-javascript.htm
+export function setAbortableTimeout(
+	callback: () => void,
+	ms: number,
+	signal: AbortSignal,
+	aborted?: () => void
+) {
+	signal?.addEventListener("abort", handleAbort, { once: true })
+
+	const timeout = setTimeout(internalCallback, ms)
+
+	function internalCallback() {
+		signal?.removeEventListener("abort", handleAbort)
+		callback()
+	}
+
+	function handleAbort() {
+		clearTimeout(timeout)
+		aborted?.()
+	}
+}
