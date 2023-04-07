135290d53b7d670903f8edc9c1de304838b36bc3
LordTocs <Tocs1001@gmail.com>
2023-04-07 12:47:45 -0400

    Reorganize the EmoteService.
    7TV updated it's UI, so we updated to v3 to support it.
    7TV changed it's CDN

--- packages/castmate-overlay-components/src/utils/emotes.js
+++ packages/castmate-overlay-components/src/utils/emotes.js
@@ -1,17 +1,12 @@
-//BTTV List
+//BTTV List https://betterttv.com/developers/api
 //https://api.betterttv.net/3/cached/users/twitch/27082158
 //BTTV CDN
 //https://cdn.betterttv.net/emote/<emote id>/3x
 
-//FFZ
-// https://api.frankerfacez.com/v1/set/global set 0
-// https://api.frankerfacez.com/v1/room/id/27082158
-// https://cdn.frankerfacez.com/emote/128054/3
+//FFZ https://api.frankerfacez.com/docs/
 
-//7TTV List
-// https://api.7tv.app/v2/users/lordtocs/emotes
-// SSE https://events.7tv.app/v1/channel-emotes?channel=lordtocs
-// https://cdn.7tv.app/emote/60e5cded840f3a570108cd8d/1x
+//7TV https://7tv.io/docs
+//7TV Events https://github.com/SevenTV/EventAPI
 
 import joypixels from "emoji-toolkit"
 import axios from "axios"
@@ -21,140 +16,154 @@ let unicodeRegExp = new RegExp(
 	"gi"
 )
 
-export class EmoteService {
-	constructor() {
-		this.bttvLookup = {}
-		this.sevenTVLookup = {}
-		this.ffzLookup = {}
-		this.thirdPartyLookup = {}
+class BTTVService {
+	constructor(onUpdate) {
+		this.onUpdate = onUpdate
 	}
 
-	async init(channelId, channelName) {
-		this.channelName = channelName
-		this.channelId = channelId
-
-		if (channelId && channelName) {
-			await this.initBTTV()
-
-			await this.init7tv()
-
-			await this.initFFZ()
-		}
-	}
-
-	async updateChannelId(channelId) {
-		this.channelId = channelId
-
-		await this.initBTTV()
-
-		await this.initFFZ()
-	}
-
-	async updateChannelName(channelName) {
-		this.channelName = channelName
-
-		await this.init7tv()
-	}
-
-	async initBTTV() {
-		const queryBttv = async () => {
-			const resp = await axios.get(
-				`https://api.betterttv.net/3/cached/users/twitch/${this.channelId}`
-			)
-
-			const emoteLookup = {}
-
-			const sharedEmotes = resp.data.sharedEmotes
-			const channelEmotes = resp.data.channelEmotes
-
-			for (let emote of [...sharedEmotes, ...channelEmotes]) {
-				emoteLookup[
-					emote.code
-				] = `https://cdn.betterttv.net/emote/${emote.id}/3x`
-			}
-
-			this.bttvLookup = emoteLookup
-			this.mergeLookups()
-		}
-
-		if (this.bttvReferesh) {
-			clearInterval(this.bttvReferesh)
-		}
-		this.bttvReferesh = setInterval(queryBttv, 60000)
-		await queryBttv()
-	}
-
-	async init7tv() {
-		if (this.sevenEvents) {
-			this.sevenEvents.close()
-		}
-
+	async query() {
 		const resp = await axios.get(
-			`https://api.7tv.app/v2/users/${this.channelName}/emotes`
+			`https://api.betterttv.net/3/cached/users/twitch/${this.channelId}`
+		)
+
+		const emoteLookup = {}
+
+		const sharedEmotes = resp.data.sharedEmotes
+		const channelEmotes = resp.data.channelEmotes
+
+		for (let emote of [...sharedEmotes, ...channelEmotes]) {
+			emoteLookup[
+				emote.code
+			] = `https://cdn.betterttv.net/emote/${emote.id}/3x`
+		}
+
+		const globalResp = await axios.get(`https://api.betterttv.net/3/cached/emotes/global`)
+		for (let emote of globalResp.data) {
+			emoteLookup[
+				emote.code
+			] = `https://cdn.betterttv.net/emote/${emote.id}/3x`
+		}
+		this.lookup = emoteLookup
+		this.onUpdate?.()
+	}
+
+	async init(channelId) {
+		this.channelId = channelId
+		await this.query()
+		if (this.refresh) {
+			clearInterval(this.refresh)
+		}
+		this.refresh = setInterval(() => this.query(), 60000)
+	}
+
+	get(name) {
+		return this.lookup?.[name]
+	}
+}
+
+class FFZService {
+	constructor(onUpdate) {
+		this.onUpdate = onUpdate
+	}
+
+	async query() {
+		const resp = await axios.get(
+			`https://api.frankerfacez.com/v1/room/id/${this.channelId}`
 		)
 
 		const lookup = {}
-		for (let emote of resp.data) {
-			lookup[emote.name] = `https://cdn.7tv.app/emote/${emote.id}/3x`
+		for (let setId in resp.data.sets) {
+			const set = resp.data.sets[setId]
+			for (let emote of set.emoticons) {
+				lookup[emote.name] = emote.urls["4"] || emote.urls["3"]
+			}
 		}
 
 		const globalResp = await axios.get(
-			`https://api.7tv.app/v2/emotes/global`
+			`https://api.frankerfacez.com/v1/set/global`
 		)
-		for (let emote of globalResp.data) {
-			lookup[emote.name] = `https://cdn.7tv.app/emote/${emote.id}/3x`
+		for (let setId in globalResp.data.sets) {
+			const set = globalResp.data.sets[setId]
+			for (let emote of set.emoticons) {
+				lookup[emote.name] = emote.urls["4"] || emote.urls["3"]
+			}
 		}
 
-		this.sevenTVLookup = lookup
-		this.mergeLookups()
+		this.lookup = lookup
+		this.onUpdate?.()
+	}
 
-		if (this.sevenEvents) {
-			this.sevenEvents.close()
+	async init(channelId) {
+		this.channelId = channelId
+		await this.query()
+		if (this.refresh) {
+			clearInterval(this.refresh)
+		}
+		this.refresh = setInterval(() => this.query(), 60000)
+	}
+
+	get(name) {
+		return this.lookup?.[name]
+	}
+}
+
+class SevenTVService {
+	constructor(onUpdate) {
+		this.onUpdate = onUpdate
+	}
+
+	async connect() {
+		if (this.events) {
+			this.events.close()
 		}
 
-		this.sevenEvents = new EventSource(
-			`https://events.7tv.app/v1/channel-emotes?channel=${this.channelName}`
+		this.events = new EventSource(
+			`https://events.7tv.io/v3@emote_set.update<object_id=${this.emoteSet.id}>`
 		)
 
-		this.sevenEvents.addEventListener(
-			"ready",
-			(e) => {
-				// Should be "7tv-event-sub.v1" since this is the `v1` endpoint
-				console.log("Ready", e.data)
-			},
-			false
-		)
+		this.events.addEventListener("hello", (e) => {
+			console.log("7tv: HELLO!")
+		})
 
-		this.sevenEvents.addEventListener(
-			"update",
-			(e) => {
-				const data = JSON.parse(e.data)
-				// This is a JSON payload matching the type for the specified event channel
-				if (data.action == "ADD") {
-					this.sevenTVLookup[
-						data.name
-					] = `https://cdn.7tv.app/emote/${data.emote_id}/3x`
-					console.log("Added 7tv" + data.name)
-					this.mergeLookups()
-				} else if (data.action == "REMOVE") {
-					delete this.sevenTVLookup[data.name]
-					console.log("Removed 7tv" + data.name)
-					this.mergeLookups()
+		this.events.addEventListener("dispatch", (e) => {
+			const message = JSON.parse(e.data)
+
+			if (message.type == "emote_set.update") {
+				const body = message.body
+
+				console.log("7tv Update", message.body)
+
+				if (body.id == this.emoteSet.id) {
+
+					if (body.pulled) {
+						//TODO: I'm unsure how this works with multiple pulled
+						let offset = 0
+						for (let emote of body.pulled) {
+							console.log("7tv remove: ", emote)
+							this.emoteSet.emotes.splice(emote.index - offset, 1)
+							offset++
+						}
+					}
+
+					if (body.pushed) {
+						for (let emote of body.pushed) {
+							console.log("7tv add: ", emote)
+							this.emoteSet.emotes.push(emote.value)
+						}
+					}
+
+					const lookup = {}
+					for (let emote of [...this.globalEmoteSet?.emotes, ...this.emoteSet?.emotes]) {
+						lookup[emote.name] = `https://cdn.7tv.app/emote/${emote.id}/3x.webp`
+					}
+					this.lookup = lookup
+
+					this.onUpdate?.()
 				}
-			},
-			false
-		)
+			}
+		})
 
-		this.sevenEvents.addEventListener(
-			"open",
-			(e) => {
-				// Connection was opened.
-				console.log("Open", e.data)
-			},
-			false
-		)
-
-		this.sevenEvents.addEventListener(
+		this.events.addEventListener(
 			"error",
 			(e) => {
 				if (e.readyState === EventSource.CLOSED) {
@@ -166,47 +175,71 @@ export class EmoteService {
 		)
 	}
 
-	async initFFZ() {
-		const queryFFZ = async () => {
-			const resp = await axios.get(
-				`https://api.frankerfacez.com/v1/room/id/${this.channelId}`
-			)
+	async query() {
+		const resp = await axios.get(
+			`https://api.7tv.app/v3/users/twitch/${this.channelId}`
+		)
 
-			const lookup = {}
-			for (let setId in resp.data.sets) {
-				const set = resp.data.sets[setId]
-				for (let emote of set.emoticons) {
-					lookup[emote.name] = emote.urls["4"] || emote.urls["3"]
-				}
-			}
+		const { emote_set: emoteSet } = resp.data
 
-			const globalResp = await axios.get(
-				`https://api.frankerfacez.com/v1/set/global`
-			)
-			for (let setId in globalResp.data.sets) {
-				const set = globalResp.data.sets[setId]
-				for (let emote of set.emoticons) {
-					lookup[emote.name] = emote.urls["4"] || emote.urls["3"]
-				}
-			}
+		const globalResp = await axios.get("https://7tv.io/v3/emote-sets/global")
+		const globalEmoteSet = globalResp.data
 
-			this.ffzLookup = lookup
-			this.mergeLookups()
+		this.emoteSet = emoteSet
+		console.log("7tv Emote Set", this.emoteSet)
+		this.globalEmoteSet =  globalEmoteSet
+	
+		console.log(globalEmoteSet.emotes[0])
+
+		const lookup = {}
+		for (let emote of [...globalEmoteSet?.emotes, ...emoteSet?.emotes]) {
+			lookup[emote.name] = `https://cdn.7tv.app/emote/${emote.id}/3x.webp`
 		}
-		await queryFFZ()
+		this.lookup = lookup
+		this.onUpdate?.()
+	}
 
-		if (this.ffzReferesh) {
-			clearInterval(this.ffzReferesh)
+	async init(channelId) {
+		this.channelId = channelId
+		await this.query()
+		await this.connect()
+	}
+
+	get(name) {
+		return this.lookup?.[name]
+	}
+}
+
+export class EmoteService {
+	constructor() {
+		this.ffz = new FFZService(() => this.mergeLookups())
+		this.bttv = new BTTVService(() => this.mergeLookups())
+		this.seventv = new SevenTVService(() => this.mergeLookups())
+	}
+
+	async init(channelId, channelName) {
+		this.channelName = channelName
+		this.channelId = channelId
+
+		if (channelId && channelName) {
+			await Promise.all([this.ffz.init(channelId), this.bttv.init(channelId), this.seventv.init(channelId)].map(p => p.catch((e) => console.error(e))))
 		}
-		this.ffzReferesh = setInterval(queryFFZ, 60000)
-		await queryFFZ()
+	}
+
+	async updateChannelId(channelId) {
+		this.channelId = channelId
+
+		await Promise.all([this.ffz.init(channelId), this.bttv.init(channelId), this.seventv.init(channelId)].map(p => p.catch((e) => console.error(e))))
+	}
+
+	async updateChannelName(channelName) {
 	}
 
 	mergeLookups() {
 		this.thirdPartyLookup = {
-			...this.bttvLookup,
-			...this.sevenTVLookup,
-			...this.ffzLookup,
+			...this.bttv?.lookup,
+			...this.seventv?.lookup,
+			...this.ffz?.lookup,
 		}
 	}
 
