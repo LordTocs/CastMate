fc61dfb6a96af4b20c3c8c5c70f8744171632c47
LordTocs <Tocs1001@gmail.com>
2023-07-31 17:25:43 -0400

    Hopefully implement Elgato Lightstrip support
    Todo: Have Johannes test

--- packages/castmate/src/main/plugins/elgato.js
+++ packages/castmate/src/main/plugins/elgato.js
@@ -4,6 +4,7 @@ import { IoTProvider, Light, Plug } from "../iot/iot-manager"
 import { reactify } from "../state/reactive"
 import * as chromatism from "chromatism2"
 import axios from "axios"
+import util from "util"
 
 function elgatoToKelvin(value) {
 	return Math.round((-4100 * value) / 201 + 1993300 / 201)
@@ -100,6 +101,108 @@ class ElgatoKeyLight extends Light {
 	}
 }
 
+class ElgatoLightStrip extends Light {
+	/**
+	 *
+	 * @param {bonjour.RemoteService} service
+	 */
+	constructor(service) {
+		super()
+
+		const ip = service.referer.address
+		const port = service.port
+		this.axios = axios.create({
+			baseURL: `http://${ip}:${port}/elgato`,
+		})
+		this.id = "elgato." + service.txt.id
+
+		this.state = reactify({
+			on: false,
+			color: null,
+		})
+	}
+
+	startPolling() {
+		this.poll()
+		setInterval(() => this.poll(), 30 * 1000)
+	}
+
+	async poll() {
+		const lightState = await this.axios.get("lights")
+
+		const state = lightState.data.lights[0]
+		this.state.on = state.on
+
+		const bri = state.brightness
+		if (state.temperature) {
+			const kelvin = elgatoToKelvin(state.temperature)
+			this.state.color = { bri, kelvin }
+		} else {
+			this.state.color = { bri, hue: state.hue, sat: state.saturation }
+		}
+	}
+
+	async init() {
+		const info = await this.axios.get("accessory-info")
+		const lights = await this.axios.get("lights")
+
+		this.config = {
+			name: info.data.displayName,
+			plugin: "elgato",
+			type: "bulb",
+			rgb: {
+				available: true,
+			},
+			kelvin: {
+				available: true,
+				min: elgatoToKelvin(143),
+				max: elgatoToKelvin(344),
+			},
+			dimming: {
+				available: true,
+			},
+			numberOfLights: lights.numberOfLights,
+		}
+
+		this.startPolling()
+	}
+
+	async setLightState(on, color, duration) {
+		if (on == "toggle") {
+			on = !this.state.on
+		}
+		on = on ?? this.state.on
+
+		const brightness = color?.bri ?? this.state.color?.bri ?? 100
+
+		const lightState = {
+			on,
+			brightness,
+		}
+
+		this.state.on = on
+
+		if (color) {
+			if ("kelvin" in color) {
+				const kelvin = color?.kelvin ?? this.state.color?.kelvin ?? 3000
+				lightState.temperature = kelvinToElgato(kelvin)
+				this.state.color = { bri: brightness, kelvin }
+			} else if ("sat" in color || "hue" in color) {
+				const sat = color?.sat ?? this.state.color?.sat ?? 100
+				const hue = color?.hue ?? this.state.color?.hue ?? 0
+				lightState.saturation = sat
+				lightState.hue = hue
+				this.state.color = { bri: brightness, sat, hue }
+			}
+		}
+
+		await this.axios.put("lights", {
+			numberOfLights: this.config.numberOfLights,
+			lights: new Array(this.config.numberOfLights).fill(lightState),
+		})
+	}
+}
+
 class ElgatoIoTProvider extends IoTProvider {
 	constructor(pluginObj) {
 		super("elgato")
@@ -111,14 +214,28 @@ class ElgatoIoTProvider extends IoTProvider {
 		this.keylightBrowser = this.bonjour.find(
 			{ type: "elg" },
 			async (service) => {
-				const light = new ElgatoKeyLight(service)
-				try {
-					//console.log("Elgato Light!")
-					await light.init()
+				console.log(util.inspect(service))
+				if (service.txt.md.includes("Key")) {
+					const light = new ElgatoKeyLight(service)
+					try {
+						//console.log("Elgato Light!")
+						await light.init()
 
-					await this._addNewLight(light)
-				} catch (err) {
-					console.error(err)
+						await this._addNewLight(light)
+					} catch (err) {
+						console.error(err)
+					}
+				} else {
+					//Light strip?
+					const light = new ElgatoLightStrip(service)
+					try {
+						//console.log("Elgato Light!")
+						await light.init()
+
+						await this._addNewLight(light)
+					} catch (err) {
+						console.error(err)
+					}
 				}
 			}
 		)
