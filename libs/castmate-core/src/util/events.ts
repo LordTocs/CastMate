f5a3a5205d76ee80243cd1cd026ee1421f55f28a
LordTocs <Tocs1001@gmail.com>
2023-08-06 13:48:52 -0400

    Allow events to unregister

--- libs/castmate-core/src/util/events.ts
+++ libs/castmate-core/src/util/events.ts
@@ -5,6 +5,13 @@ export class EventList {
 		this.list.push(func)
 	}
 
+	unregister(func: () => Promise<void> | void) {
+		const idx = this.list.findIndex((li) => li === func)
+		if (idx > 0) {
+			this.list.splice(idx, 1)
+		}
+	}
+
 	async run() {
 		const promises = this.list.map((f) => f())
 		await Promise.all(promises)
