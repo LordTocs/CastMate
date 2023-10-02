e30a8324ffb6d229cbbd9189bcf93b6e08e95c25
LordTocs <Tocs1001@gmail.com>
2023-10-02 13:03:58 -0400

    Stub out discord plugin

--- plugins/discord/main/src/main.ts
+++ plugins/discord/main/src/main.ts
@@ -1,13 +1,66 @@
-import { defineAction, defineTrigger, onLoad, onUnload, definePlugin } from "castmate-core"
+import { DiscordWebhookConfig } from "castmate-plugin-discord-shared"
+import {
+	defineAction,
+	defineTrigger,
+	onLoad,
+	onUnload,
+	definePlugin,
+	FileResource,
+	ResourceStorage,
+	definePluginResource,
+} from "castmate-core"
+import { WebhookClient } from "discord.js"
+
+class DiscordWebHook extends FileResource<DiscordWebhookConfig> {
+	static resourceDirectory = "./discord/webhooks"
+	static storage = new ResourceStorage<DiscordWebHook>("DiscordWebhook")
+
+	client: WebhookClient
+
+	async load(savedConfig: DiscordWebhookConfig): Promise<boolean> {
+		this.client = new WebhookClient({ url: savedConfig.webhookUrl })
+		return await super.load(savedConfig)
+	}
+
+	async setConfig(config: DiscordWebhookConfig): Promise<boolean> {
+		this.client = new WebhookClient({ url: config.webhookUrl })
+		return await super.setConfig(config)
+	}
+
+	async applyConfig(config: Partial<DiscordWebhookConfig>): Promise<boolean> {
+		if (!(await super.applyConfig(config))) return false
+		this.client = new WebhookClient({ url: this.config.webhookUrl })
+		return true
+	}
+}
 
 export default definePlugin(
 	{
 		id: "discord",
-		name: "UI Name",
+		name: "Discord",
 		description: "UI Description",
 		icon: "mdi mdi-discord",
+		color: "#7289da",
 	},
 	() => {
-		//Plugin Intiialization
+		definePluginResource(DiscordWebHook)
+
+		defineAction({
+			id: "discordMessage",
+			name: "Discord Message",
+			icon: "mdi mdi-message",
+			config: {
+				type: Object,
+				properties: {
+					webhook: { type: DiscordWebHook, name: "Channel Webhook", required: true },
+					message: { type: String, name: "Message", required: true, default: "", template: true },
+				},
+			},
+			async invoke(config, contextData, abortSignal) {
+				await config.webhook?.client?.send({
+					content: config.message,
+				})
+			},
+		})
 	}
 )
---
+++ plugins/discord/shared/src/main.ts
@@ -0,0 +1,3 @@
+export interface DiscordWebhookConfig {
+	webhookUrl: string
+}
