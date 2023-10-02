0ce712af9eb0edc5063d1dd1420971c3977b2bda
LordTocs <Tocs1001@gmail.com>
2023-10-01 23:15:45 -0400

    Add Delay to the time plugin

--- plugins/time/main/src/main.ts
+++ plugins/time/main/src/main.ts
@@ -1,13 +1,35 @@
-import { defineAction, defineTrigger, onLoad, onUnload, definePlugin } from "castmate-core"
+import { Duration } from "castmate-schema"
+import { defineAction, defineTrigger, onLoad, onUnload, definePlugin, abortableSleep } from "castmate-core"
 
 export default definePlugin(
 	{
 		id: "time",
 		name: "Time",
 		description: "Time Utilities",
-		icon: "mdi-pencil",
+		icon: "mdi mdi-clock-outline",
 	},
 	() => {
 		//Plugin Intiialization
+
+		defineAction({
+			id: "delay",
+			name: "Delay",
+			icon: "mdi mdi-timer-sand",
+			duration: {
+				dragType: "length",
+				rightSlider: {
+					sliderProp: "duration",
+				},
+			},
+			config: {
+				type: Object,
+				properties: {
+					duration: { type: Duration, name: "Duration", template: true, required: true, default: 1.0 },
+				},
+			},
+			async invoke(config, contextData, abortSignal) {
+				await abortableSleep(config.duration * 1000, abortSignal)
+			},
+		})
 	}
 )
