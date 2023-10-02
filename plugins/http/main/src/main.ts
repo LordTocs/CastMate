46345779859208e02a7dfaee32a6f1fedb7875c6
LordTocs <Tocs1001@gmail.com>
2023-10-02 13:06:13 -0400

    Stub Out HTTP Request Action

--- plugins/http/main/src/main.ts
+++ plugins/http/main/src/main.ts
@@ -1,13 +1,52 @@
 import { defineAction, defineTrigger, onLoad, onUnload, definePlugin } from "castmate-core"
-
+import axios from "axios"
 export default definePlugin(
 	{
 		id: "http",
-		name: "UI Name",
+		name: "HTTP",
 		description: "UI Description",
-		icon: "mdi-pencil",
+		icon: "mdi mdi-web",
+		color: "#9E436E",
 	},
 	() => {
-		//Plugin Intiialization
+		defineAction({
+			id: "request",
+			name: "HTTP Request",
+			icon: "mdi mdi-web",
+			config: {
+				type: Object,
+				properties: {
+					method: {
+						type: String,
+						name: "Method",
+						enum: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+						required: true,
+						default: "GET",
+					},
+					url: {
+						type: String,
+						template: true,
+						name: "URL",
+						required: true,
+					},
+					//TODO: Query
+					//TODO: Headers
+					//TODO: Body
+				},
+			},
+			result: {
+				type: Object,
+				properties: {},
+			},
+			async invoke(config, contextData, abortSignal) {
+				//TODO: Cancel Token
+				const resp = await axios.request({
+					method: config.method,
+					url: config.url,
+				})
+
+				return resp.data
+			},
+		})
 	}
 )
