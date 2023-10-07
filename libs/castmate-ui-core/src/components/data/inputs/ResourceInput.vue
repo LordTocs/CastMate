bd8c29d9dc33bd1337922c576b1afa2266634062
LordTocs <Tocs1001@gmail.com>
2023-10-07 18:43:42 -0400

    Add Edit Menus to Resource Inputs

--- libs/castmate-ui-core/src/components/data/inputs/ResourceInput.vue
+++ libs/castmate-ui-core/src/components/data/inputs/ResourceInput.vue
@@ -1,21 +1,34 @@
 <template>
-	<c-autocomplete
-		v-model="model"
-		:required="!!schema.required"
-		:label="schema.name"
-		text-prop="config.name"
-		:items="resourceItems"
-		input-id="resource"
-		:no-float="noFloat"
-	/>
+	<div class="w-full flex flex-row">
+		<c-autocomplete
+			v-model="model"
+			:required="!!schema.required"
+			:label="schema.name"
+			text-prop="config.name"
+			:items="resourceItems"
+			input-id="resource"
+			:no-float="noFloat"
+		/>
+		<p-button
+			class="ml-1"
+			text
+			icon="mdi mdi-dots-vertical"
+			v-if="hasDialogs"
+			aria-controls="overlay_menu"
+			@click="menu?.toggle($event)"
+		></p-button>
+		<p-menu ref="menu" id="overlay_menu" :model="menuItems" :popup="true" v-if="hasDialogs" />
+	</div>
 </template>
 
 <script setup lang="ts">
 import { SchemaResource, ResourceData } from "castmate-schema"
 import { ResourceProxy } from "../../../util/data"
 import { computed, nextTick, ref, useModel, watch } from "vue"
-import { useResourceArray, useResourceData } from "../../../main"
-
+import { useResourceArray, useResourceCreateDialog, useResourceData, useResourceEditDialog } from "../../../main"
+import PButton from "primevue/button"
+import PMenu from "primevue/menu"
+import { MenuItem } from "primevue/menuitem"
 import _clamp from "lodash/clamp"
 import { SharedDataInputProps } from "../DataInputTypes"
 
@@ -32,6 +45,33 @@ const model = useModel(props, "modelValue")
 const resourceStore = useResourceData(() => props.schema.resourceType)
 
 const resourceItems = useResourceArray(() => props.schema.resourceType)
+
+const hasDialogs = computed(() => resourceStore.value?.editDialog && resourceStore.value?.createDialog)
+
+const createResourceDlg = useResourceCreateDialog(() => props.schema.resourceType)
+const editResourceDlg = useResourceEditDialog(() => props.schema.resourceType)
+
+const menu = ref<InstanceType<typeof PMenu>>()
+const menuItems = computed<MenuItem[]>(() => {
+	const selectedResource = props.modelValue ? resourceStore.value?.resources?.get(props.modelValue) : undefined
+
+	return [
+		{
+			label: `Create New`,
+			command(event) {
+				createResourceDlg() //TODO: Select the item after it's been created
+			},
+		},
+		{
+			label: `Edit ${selectedResource?.config?.name ?? selectedResource?.id ?? props.schema.resourceType}`,
+			disabled: !selectedResource,
+			command(event) {
+				if (!props.modelValue) return
+				editResourceDlg(props.modelValue)
+			},
+		},
+	]
+})
 </script>
 
 <style scoped>
