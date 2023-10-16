5105005ea04c80f62f156c32294b916b77d082d0
LordTocs <Tocs1001@gmail.com>
2023-10-15 23:49:13 -0400

    Add DelayedResolver

--- libs/castmate-core/src/util/abort-utils.ts
+++ libs/castmate-core/src/util/abort-utils.ts
@@ -63,3 +63,20 @@ export function abortablePromise<T>(
 		)
 	})
 }
+
+export interface DelayedResolver<T> {
+	resolve: (result: T | PromiseLike<T>) => any
+	reject: (reason?: any) => any
+	promise: Promise<T>
+}
+
+export function createDelayedResolver<T>() {
+	const store: Record<string, any> = {}
+
+	store.promise = new Promise<T>((resolve, reject) => {
+		store.resolve = resolve
+		store.reject = reject
+	})
+
+	return store as DelayedResolver<T>
+}
