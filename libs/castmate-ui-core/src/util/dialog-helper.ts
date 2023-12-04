9ff64b64ced7094a65ee3e7fc3c3007298f0923b
LordTocs <Tocs1001@gmail.com>
2023-12-04 16:14:30 -0500

    Add dummy dialog helper type to simplify undefined usage

--- libs/castmate-ui-core/src/util/dialog-helper.ts
+++ libs/castmate-ui-core/src/util/dialog-helper.ts
@@ -1,8 +1,11 @@
 import { DialogServiceMethods } from "primevue/dialogservice"
-import { inject, type Ref } from "vue"
+import { inject, type Ref, computed } from "vue"
 
 export type DynamicDialogInstance = ReturnType<DialogServiceMethods["open"]>
 
 export function useDialogRef() {
-	return inject<Ref<DynamicDialogInstance | undefined>>("dialogRef")
+	return inject<Ref<DynamicDialogInstance | undefined>>(
+		"dialogRef",
+		computed(() => undefined)
+	)
 }
