43092f94f1dda82e06a55408715047645a2692cd
LordTocs <Tocs1001@gmail.com>
2024-01-07 20:02:33 -0500

    Add timeout, ban, and unban

--- plugins/twitch/main/src/moderation.ts
+++ plugins/twitch/main/src/moderation.ts
@@ -1,6 +1,9 @@
+import { defineAction } from "castmate-core"
 import { onChannelAuth } from "./api-harness"
 import { TwitchAccount } from "./twitch-auth"
 import { ViewerCache } from "./viewer-cache"
+import { TwitchViewer } from "castmate-plugin-twitch-shared"
+import { Duration } from "castmate-schema"
 
 export function setupModeration() {
 	onChannelAuth((channel, service) => {
@@ -11,5 +14,60 @@ export function setupModeration() {
 		service.eventsub.onChannelModeratorRemove(channel.twitchId, (event) => {
 			ViewerCache.getInstance().setIsMod(event.userId, false)
 		})
+	})
+
+	defineAction({
+		id: "timeout",
+		name: "Timeout Viewer",
+		icon: "mdi mdi-timer-remove-outline",
+		config: {
+			type: Object,
+			properties: {
+				viewer: { type: TwitchViewer, name: "Viewer", required: true },
+				duration: { type: Duration, name: "Duration", required: true, template: true },
+				reason: { type: String, name: "Reason", template: true },
+			},
+		},
+		async invoke(config, contextData, abortSignal) {
+			await TwitchAccount.channel.apiClient.moderation.banUser(TwitchAccount.channel.twitchId, {
+				user: config.viewer,
+				duration: config.duration,
+				reason: config.reason ?? "",
+			})
+		},
+	})
+
+	defineAction({
+		id: "ban",
+		name: "Ban Viewer",
+		icon: "mdi mdi-cancel",
+		config: {
+			type: Object,
+			properties: {
+				viewer: { type: TwitchViewer, name: "Viewer", required: true },
+				reason: { type: String, name: "Reason", template: true },
+			},
+		},
+		async invoke(config, contextData, abortSignal) {
+			await TwitchAccount.channel.apiClient.moderation.banUser(TwitchAccount.channel.twitchId, {
+				user: config.viewer,
+				reason: config.reason ?? "",
+			})
+		},
+	})
+
+	defineAction({
+		id: "unban",
+		name: "Unban Viewer",
+		icon: "mdi mdi-cancel",
+		config: {
+			type: Object,
+			properties: {
+				viewer: { type: TwitchViewer, name: "Viewer", required: true },
+			},
+		},
+		async invoke(config, contextData, abortSignal) {
+			await TwitchAccount.channel.apiClient.moderation.unbanUser(TwitchAccount.channel.twitchId, config.viewer)
+		},
 	})
 }
