c19ba88582e0327374ac5ff78edd44b136252a0b
Scott Resnick <ScottCResnick@gmail.com>
2024-02-10 19:17:54 -0500

    Add helper function for formating time durations to a string

--- libs/castmate-schema/src/data/duration.ts
+++ libs/castmate-schema/src/data/duration.ts
@@ -26,3 +26,61 @@ declare module "../schema" {
 registerType("Duration", {
 	constructor: Duration,
 })
+
+const HOUR_DUR = 60 * 60
+const MINUTE_DUR = 60
+export function formatDuration(duration: Duration | undefined, decimalPlaces: number = 4) {
+	if (duration == null) {
+		return ""
+	}
+
+	let hours = 0
+	let minutes = 0
+	let seconds = 0
+
+	let remaining = duration as number
+	if (remaining > HOUR_DUR) {
+		hours = Math.floor(remaining / HOUR_DUR)
+		remaining = remaining % HOUR_DUR
+	}
+	if (remaining > MINUTE_DUR) {
+		minutes = Math.floor(remaining / MINUTE_DUR)
+		remaining = remaining % MINUTE_DUR
+	}
+	seconds = remaining
+
+	let result = ""
+	if (hours > 0) {
+		result += hours + "h "
+
+		result +=
+			minutes.toLocaleString("en-Us", {
+				minimumIntegerDigits: 2,
+				useGrouping: false,
+			}) + "m "
+
+		result +=
+			seconds.toLocaleString("en-Us", {
+				minimumIntegerDigits: 2,
+				maximumFractionDigits: decimalPlaces,
+				useGrouping: false,
+			}) + "s"
+	} else if (minutes > 0) {
+		result += minutes + "m "
+
+		result +=
+			seconds.toLocaleString("en-Us", {
+				minimumIntegerDigits: 2,
+				maximumFractionDigits: decimalPlaces,
+				useGrouping: false,
+			}) + "s"
+	} else {
+		result +=
+			seconds.toLocaleString("en-Us", {
+				maximumFractionDigits: decimalPlaces,
+				useGrouping: false,
+			}) + "s"
+	}
+
+	return result
+}
