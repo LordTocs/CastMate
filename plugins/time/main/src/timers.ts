85c6b5d9f708d1e3d9d78841e36265c795e27f68
Scott Resnick <ScottCResnick@gmail.com>
2024-02-11 15:39:54 -0500

    Add timer trigger

--- plugins/time/main/src/timers.ts
+++ plugins/time/main/src/timers.ts
@@ -1,12 +1,16 @@
 import {
+	ReactiveEffect,
+	autoRerun,
 	defineAction,
 	defineTrigger,
 	onProfilesChanged,
 	registerSchemaCompare,
 	registerSchemaExpose,
 	scheduleReactiveTrigger,
+	usePluginLogger,
 } from "castmate-core"
-import { Timer, getTimeRemaining, isTimer } from "castmate-plugin-time-shared"
+import { Timer, getTimeRemaining, isTimer, isTimerStarted } from "castmate-plugin-time-shared"
+import { VariableManager } from "castmate-plugin-variables-main"
 import { Duration, ValueCompareOperator } from "castmate-schema"
 
 export function compareTimer(lhs: Timer, rhs: any, operator: ValueCompareOperator) {
@@ -66,6 +70,8 @@ interface RepeatingTimer {
 }
 
 export function setupTimers() {
+	const logger = usePluginLogger()
+
 	function stopRepeatingTimer(repeat: RepeatingTimer) {
 		if (repeat.delay) {
 			clearTimeout(repeat.delay)
@@ -150,6 +156,111 @@ export function setupTimers() {
 					stopRepeatingTimer(timer)
 					repeatingTimers.delete(slug)
 				}
+			}
+		}
+	})
+
+	const timerTrigger = defineTrigger({
+		id: "timer",
+		name: "Timer",
+		icon: "mdi mdi-timer",
+		config: {
+			type: Object,
+			properties: {
+				timer: {
+					type: String,
+					name: "Timer",
+					required: true,
+					async enum() {
+						return VariableManager.getInstance()
+							.variableDefinitions.filter((v) => v.schema.type == Timer)
+							.map((v) => v.id)
+					},
+				},
+				offset: {
+					type: Duration,
+					name: "Time Remaining",
+					required: true,
+					default: 0,
+				},
+			},
+		},
+		context: {
+			type: Object,
+			properties: {
+				triggerId: { type: String, required: true },
+				profileId: { type: String, required: true },
+			},
+		},
+		async handle(config, context, mapping) {
+			if (mapping.profileId != context.profileId) return false
+			if (mapping.triggerId != context.triggerId) return false
+			return true
+		},
+	})
+
+	interface TimerTriggerer {
+		profileId: string
+		triggerId: string
+		timeout?: NodeJS.Timeout
+		effect?: ReactiveEffect
+	}
+
+	function disposeTimerTriggerer(triggerer: TimerTriggerer) {
+		if (triggerer.timeout) {
+			clearTimeout(triggerer.timeout)
+			triggerer.timeout = undefined
+		}
+
+		if (triggerer.effect) {
+			triggerer.effect.dispose()
+			triggerer.effect = undefined
+		}
+	}
+
+	let timerTriggerers: TimerTriggerer[] = []
+
+	onProfilesChanged(async (activeProfiles, inactiveProfiles) => {
+		for (const triggerer of timerTriggerers) {
+			disposeTimerTriggerer(triggerer)
+		}
+
+		timerTriggerers = []
+
+		for (const prof of activeProfiles) {
+			for (const trigger of prof.iterTriggers(timerTrigger)) {
+				const triggerer: TimerTriggerer = {
+					profileId: prof.id,
+					triggerId: trigger.id,
+				}
+
+				timerTriggerers.push(triggerer)
+
+				triggerer.effect = await autoRerun(() => {
+					logger.log("Reorganizing Timer Trigger")
+					const timer = VariableManager.getInstance().getVariable<Timer>(trigger.config.timer)
+					if (!timer) return
+					if (!isTimer(timer.ref.value)) return
+
+					if (triggerer.timeout) {
+						clearTimeout(triggerer.timeout)
+						triggerer.timeout = undefined
+					}
+
+					if (!isTimerStarted(timer.ref.value)) return
+
+					const remaining = getTimeRemaining(timer.ref.value)
+					const timeTilTrigger = remaining - trigger.config.offset
+					if (timeTilTrigger > 0) {
+						logger.log("Setting Timer Timeout", timeTilTrigger)
+						triggerer.timeout = setTimeout(() => {
+							timerTrigger({
+								profileId: triggerer.profileId,
+								triggerId: triggerer.triggerId,
+							})
+						}, timeTilTrigger * 1000)
+					}
+				})
 			}
 		}
 	})
