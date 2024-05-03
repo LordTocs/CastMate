9c43ba6271a3eb2081f70d32bbad624f949ea4d9
LordTocs <Tocs1001@gmail.com>
2024-05-03 19:56:01 -0400

    Add Scroll Bar to Project View

--- packages/castmate/src/renderer/components/project/ProjectView.vue
+++ packages/castmate/src/renderer/components/project/ProjectView.vue
@@ -1,27 +1,13 @@
 <template>
-	<div class="project-view">
+	<flex-scroller class="project-view">
 		<project-group-or-item v-for="pi in projectStore.projectItems" :group-or-item="pi" />
-		<!-- <project-category title="Profiles" icon="mdi mdi-card-account-details-outline">
-			<project-item v-for="(profile, i) in profiles" :title="profile.name" :key="profile.id" :indent="1">
-				<template #end>
-					<toggle-input v-model="profiles[i].state" />
-				</template>
-			</project-item>
-		</project-category>
-		<project-category title="Overlays" icon="mdi mdi-picture-in-picture-top-right">
-			<project-item v-for="(overlay, i) in overlays" :title="overlay.name" :key="overlay.id" :indent="1">
-			</project-item>
-		</project-category>
-		<project-item title="SpellCast" icon="cmi cmi-spellcast" />
-		<project-item title="Media" icon="mdi mdi-multimedia" />
-		<project-item title="Channel Point Rewards" icon="mdi mdi-star-circle-outline" /> -->
-	</div>
+	</flex-scroller>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
 import ProjectGroupOrItem from "./ProjectGroupOrItem.vue"
-import { ToggleInput, useProjectStore } from "castmate-ui-core"
+import { ToggleInput, useProjectStore, FlexScroller } from "castmate-ui-core"
 import { type Toggle } from "castmate-schema"
 
 const projectStore = useProjectStore()
