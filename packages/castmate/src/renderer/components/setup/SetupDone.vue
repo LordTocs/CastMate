d645518fbbf47193889c39467e1c488fb6c4ae4c
LordTocs <Tocs1001@gmail.com>
2024-07-08 13:02:00 -0400

    Remove Todo

--- packages/castmate/src/renderer/components/setup/SetupDone.vue
+++ packages/castmate/src/renderer/components/setup/SetupDone.vue
@@ -1,7 +1,6 @@
 <template>
 	<div>
 		<h1 class="text-center mb-0">Setup Complete!</h1>
-		<p class="p-text-secondary text-center">TODO: More Info Here</p>
 		<p class="p-text-secondary text-center">Thanks for setting up CastMate!</p>
 	</div>
 </template>
