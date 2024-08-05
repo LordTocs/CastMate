faf0fcdcb4f360f5adeab1df53fcc71d7a4721ac
LordTocs <Tocs1001@gmail.com>
2024-08-05 14:37:36 -0400

    Remove Message Pump UV Idle
    We don't need it and it was absolutely smashing the CPU

--- plugins/input/native/src/native-index.cc
+++ plugins/input/native/src/native-index.cc
@@ -7,40 +7,11 @@
 
 #include "input-interface.hh"
 
-class uv_message_pump
-{
-    uv_idle_t idle;
-public:
-    uv_message_pump(Napi::Env env)
-    {
-        uv_loop_s* loop;
-        napi_get_uv_event_loop(env, &loop);
-        uv_idle_init(loop, &idle);
-        uv_idle_start(&idle, &uv_message_pump::message_pump);
-    }
-
-    ~uv_message_pump()
-    {
-        uv_idle_stop(&idle);
-    }
-
-    static void message_pump(uv_idle_t* handle)
-    {
-        MSG msg;
-        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-};
 
 class instance_data
 {
-    uv_message_pump pump;
 public:
     instance_data(Napi::Env env)
-        : pump(env)
     {
     }
 
