9991775954e5ddce338fa22cf46303048e8d6e0a
LordTocs <Tocs1001@gmail.com>
2024-10-11 21:29:03 -0400

    Add dashboards plugin

---
+++ plugins/dashboards/main/src/main.ts
@@ -0,0 +1,13 @@
+import { defineAction, defineTrigger, onLoad, onUnload, definePlugin } from "castmate-core"
+
+export default definePlugin(
+	{
+		id: "dashboards",
+		name: "Dashboards",
+		description: "DASHBOARDS",
+		icon: "mdi mdi-pencil",
+	},
+	() => {
+		//Plugin Intiialization
+	}
+)
