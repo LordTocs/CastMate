71d2340adf76b263ac68e898d4246a7357733825
LordTocs <Tocs1001@gmail.com>
2024-11-02 16:22:13 -0400

    Make dashboard pages scrollable and add page controls

--- packages/castmate-satellite/src/renderer/components/dashboard/DashboardDisplay.vue
+++ packages/castmate-satellite/src/renderer/components/dashboard/DashboardDisplay.vue
@@ -1,6 +1,12 @@
 <template>
 	<div class="flex-grow-1 flex flex-column">
-		<div class="page-header"></div>
+		<div class="page-header">
+			<p-button icon="mdi mdi-arrow-left-bold" :disabled="!hasLeft" @click="pageLeft"></p-button>
+			<div class="flex-grow-1 text-center">
+				{{ currentPage?.name }}
+			</div>
+			<p-button icon="mdi mdi-arrow-right-bold" :disabled="!hasRight" @click="pageRight"></p-button>
+		</div>
 		<dashboard-page :page="currentPage" v-if="currentPage"> </dashboard-page>
 	</div>
 </template>
@@ -11,15 +17,45 @@ import YAML from "yaml"
 import { DashboardConfig } from "castmate-plugin-dashboards-shared"
 import { computed, ref } from "vue"
 import { useDashboardRTCBridge } from "../../util/dashboard-rtc-bridge"
+import PButton from "primevue/button"
 
 const currentPageIndex = ref(0)
+
+const clampedPageIndex = computed(() => {
+	return Math.min(Math.max(currentPageIndex.value, 0), bridge.config.pages.length - 1)
+})
+
+const hasLeft = computed(() => {
+	return clampedPageIndex.value > 0
+})
+
+const hasRight = computed(() => {
+	return clampedPageIndex.value < bridge.config.pages.length
+})
 
 const bridge = useDashboardRTCBridge()
 
 const currentPage = computed(() => {
-	const clampedIdx = Math.min(Math.max(currentPageIndex.value, 0), bridge.config.pages.length - 1)
+	const clampedIdx = clampedPageIndex.value
 	return bridge.config.pages[clampedIdx]
 })
+
+function pageLeft(ev: MouseEvent) {
+	currentPageIndex.value = clampedPageIndex.value - 1
+}
+
+function pageRight(ev: MouseEvent) {
+	currentPageIndex.value = clampedPageIndex.value + 1
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.page-header {
+	background-color: var(--surface-c);
+	min-height: 3rem;
+
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+}
+</style>
--- packages/castmate-satellite/src/renderer/components/dashboard/DashboardPage.vue
+++ packages/castmate-satellite/src/renderer/components/dashboard/DashboardPage.vue
@@ -1,12 +1,13 @@
 <template>
-	<div class="dashboard-page flex-grow-1">
+	<flex-scroller class="flex-grow-1" inner-class="dashboard-page">
 		<dashboard-section v-for="section in page.sections" :key="section.id" :section="section" :page="page.id" />
-	</div>
+	</flex-scroller>
 </template>
 
 <script setup lang="ts">
 import { DashboardPage } from "castmate-plugin-dashboards-shared"
 import DashboardSection from "./DashboardSection.vue"
+import { FlexScroller } from "castmate-ui-core"
 
 const props = defineProps<{
 	page: DashboardPage
@@ -14,12 +15,12 @@ const props = defineProps<{
 </script>
 
 <style scoped>
-.dashboard-page {
+:deep(.dashboard-page) {
 	display: flex;
 	flex-direction: row;
 	flex-wrap: wrap;
 
-	align-items: center;
+	align-items: stretch;
 	justify-content: center;
 
 	padding: 1rem;
