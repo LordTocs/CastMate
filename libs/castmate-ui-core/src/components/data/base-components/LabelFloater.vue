6df2a813665b3a107160633550d7ec55a1519311
LordTocs <Tocs1001@gmail.com>
2025-01-06 16:45:31 -0500

    Import change to label floater

--- libs/castmate-ui-core/src/components/data/base-components/LabelFloater.vue
+++ libs/castmate-ui-core/src/components/data/base-components/LabelFloater.vue
@@ -1,13 +1,13 @@
 <template>
-	<float-label v-if="!noFloat" variant="on">
+	<p-float-label v-if="!noFloat" variant="on">
 		<slot :placeholder="undefined" :input-id="inputId"> </slot>
 		<label :for="inputId"> <i v-if="icon" :class="icon" /> {{ label }}</label>
-	</float-label>
+	</p-float-label>
 	<slot v-else :placeholder="label" :input-id="inputId" v-bind="$attrs"> </slot>
 </template>
 
 <script setup lang="ts">
-import { FloatLabel } from "primevue"
+import PFloatLabel from "primevue/floatlabel"
 
 const props = defineProps<{
 	noFloat?: boolean
