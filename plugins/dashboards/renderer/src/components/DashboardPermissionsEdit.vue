604ac6e26787a4ed77da34941eda362b27d009ff
LordTocs <Tocs1001@gmail.com>
2025-01-24 15:00:44 -0500

    Import Cleanup

--- plugins/dashboards/renderer/src/components/DashboardPermissionsEdit.vue
+++ plugins/dashboards/renderer/src/components/DashboardPermissionsEdit.vue
@@ -6,9 +6,9 @@
 
 <script setup lang="ts">
 import { declareSchema } from "castmate-schema"
-import { DataInput, provideScrollAttachable, useDialogRef } from "castmate-ui-core"
+import { DataInput } from "castmate-ui-core"
 import { TwitchViewer, TwitchViewerUnresolved } from "castmate-plugin-twitch-shared"
-import { onMounted, ref, useModel } from "vue"
+import { useModel } from "vue"
 
 const editSchema = declareSchema({
 	type: Array,
