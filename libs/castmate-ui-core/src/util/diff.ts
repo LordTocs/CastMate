1bdd091bccde96ac4b56d115f8bd9ab6409dfbc0
LordTocs <Tocs1001@gmail.com>
2025-02-04 23:31:27 -0500

    Stub out apply and applyInv diff functions

--- libs/castmate-ui-core/src/util/diff.ts
+++ libs/castmate-ui-core/src/util/diff.ts
@@ -41,6 +41,80 @@ export function computeDataDiff(a: any, b: any): DataDiff | undefined {
 	return undefined
 }
 
+function applyDataDiffArray(arr: any, diff: ArrayDiff) {
+	if (!Array.isArray(arr)) throw new Error("Diff Type Mismatch")
+	for (const op of diff.ops) {
+		if (op.type == "delete") {
+			arr.splice(op.index, 1)
+		} else if (op.type == "insert") {
+			arr.splice(op.index, 0, op.value)
+		} else if (op.type == "diff") {
+			applyDataDiff(arr, op.index, op.diff)
+		}
+	}
+}
+
+function applyDataDiffObject(obj: any, diff: ObjectDiff) {
+	if (typeof obj != "object") throw new Error("Diff Type Mismatch")
+	for (const prop in diff.properties) {
+		const op = diff.properties[prop]
+		if (op.op == "added") {
+			obj[prop] = op.value
+		} else if (op.op == "removed") {
+			delete obj[prop]
+		} else if (op.op == "diff") {
+			applyDataDiff(obj, prop, op.diff)
+		}
+	}
+}
+
+export function applyDataDiff<T>(parent: T, key: keyof T, diff: DataDiff) {
+	if (diff.type == "array") {
+		applyDataDiffArray(parent[key], diff)
+	} else if (diff.type == "object") {
+		applyDataDiffObject(parent[key], diff)
+	} else if (diff.type == "generic") {
+		parent[key] = diff.newValue
+	}
+}
+
+function applyInvDataDiffArray(arr: any, diff: ArrayDiff) {
+	if (!Array.isArray(arr)) throw new Error("Diff Type Mismatch")
+	for (const op of diff.ops) {
+		if (op.type == "delete") {
+			arr.splice(op.index, 0, op.oldValue)
+		} else if (op.type == "insert") {
+			arr.splice(op.index, 1)
+		} else if (op.type == "diff") {
+			applyInvDataDiff(arr, op.index, op.diff)
+		}
+	}
+}
+
+function applyInvDataDiffObject(obj: any, diff: ObjectDiff) {
+	if (typeof obj != "object") throw new Error("Diff Type Mismatch")
+	for (const prop in diff.properties) {
+		const op = diff.properties[prop]
+		if (op.op == "added") {
+			delete obj[prop]
+		} else if (op.op == "removed") {
+			obj[prop] = op.oldValue
+		} else if (op.op == "diff") {
+			applyInvDataDiff(obj, prop, op.diff)
+		}
+	}
+}
+
+export function applyInvDataDiff<T>(parent: T, key: keyof T, diff: DataDiff) {
+	if (diff.type == "array") {
+		applyInvDataDiffArray(parent[key], diff)
+	} else if (diff.type == "object") {
+		applyInvDataDiffObject(parent[key], diff)
+	} else if (diff.type == "generic") {
+		parent[key] = diff.oldValue
+	}
+}
+
 interface GenericDiff<T = any> {
 	type: "generic"
 	oldValue: T
