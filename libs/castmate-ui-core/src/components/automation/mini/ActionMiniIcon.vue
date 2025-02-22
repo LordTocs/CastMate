6fc73753e62b4c43b388c066530e75a9c51177ac
LordTocs <Tocs1001@gmail.com>
2025-02-22 13:13:13 -0500

    Add tooltip back to mini preview

--- libs/castmate-ui-core/src/components/automation/mini/ActionMiniIcon.vue
+++ libs/castmate-ui-core/src/components/automation/mini/ActionMiniIcon.vue
@@ -1,17 +1,35 @@
 <template>
-	<div :style="{ ...actionColorStyle }" class="mini-icon">
+	<div :style="{ ...actionColorStyle }" class="mini-icon" @mouseenter="onEnter" @mouseleave="onLeave">
 		<i :class="action?.icon ?? 'mdi mdi-star'" />
 	</div>
+	<p-popover
+		v-if="highlightAction && action && Object.keys(highlightAction.config).length > 0"
+		ref="popover"
+		:pt="{
+			root: {
+				style: {
+					'--p-popover-background': actionColorStyle['--action-color'],
+					'--p-popover-border-color': actionColorStyle['--darker-action-color'],
+				},
+			},
+		}"
+	>
+		<div class="flex flex-column align-items-center justify-content-center mini-tooltip text-center">
+			<data-view :model-value="highlightAction.config" :schema="action.config" no-label />
+		</div>
+	</p-popover>
 </template>
 
 <script setup lang="ts">
 import { AnyAction } from "castmate-schema"
-import { ActionSelection, useAction, useActionColors } from "../../../main"
+import { ActionSelection, useAction, useActionColors, DataView } from "../../../main"
 import { ActionStack } from "castmate-schema"
-import { computed } from "vue"
+import { computed, ref } from "vue"
 import { isActionStack } from "castmate-schema"
+import PPopover from "primevue/popover"
 
 const props = defineProps<{
+	id: string
 	action: AnyAction | ActionStack | undefined
 }>()
 
@@ -27,9 +45,24 @@ const highlightAction = computed(() => {
 
 const action = useAction(highlightAction)
 const { actionColorStyle } = useActionColors(highlightAction, false)
+
+const popover = ref<InstanceType<typeof PPopover>>()
+
+function onEnter(ev: MouseEvent) {
+	popover.value?.show(ev)
+}
+
+function onLeave(ev: MouseEvent) {
+	popover.value?.hide()
+}
 </script>
 
 <style scoped>
+.mini-tooltip {
+	font-size: 0.7rem;
+	max-width: 10rem;
+}
+
 .mini-icon {
 	border-radius: var(--border-radius);
 	height: 1.5em;
