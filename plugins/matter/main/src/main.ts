dcac0fa679802c241ff95e3f0a8c8284da489b20
LordTocs <Tocs1001@gmail.com>
2025-05-09 18:12:25 -0400

    Initial matter plugin stuff

---
+++ plugins/matter/main/src/main.ts
@@ -0,0 +1,13 @@
+import { defineAction, defineTrigger, onLoad, onUnload, definePlugin } from "castmate-core"
+
+export default definePlugin(
+	{
+		id: "matter",
+		name: "Matter",
+		description: "Integration for open smart home standard. Matter.",
+		icon: "mdi-pencil",
+	},
+	() => {
+		//Plugin Intiialization
+	}
+)
