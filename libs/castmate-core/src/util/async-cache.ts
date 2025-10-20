e2edd8f2c2cb88c9b54311197eabb8339d6d4e69
LordTocs <Tocs1001@gmail.com>
2025-10-20 19:17:59 -0400

    Add basic async dict cache for looking up and containing keyed data

--- libs/castmate-core/src/util/async-cache.ts
+++ libs/castmate-core/src/util/async-cache.ts
@@ -5,7 +5,7 @@ export class AsyncCache<T> {
 
 	constructor(private updater: () => Promise<T>, private cacheTime = 15) {}
 
-	async _fetch() {
+	private async fetch() {
 		if (this.fetchPromise) {
 			await this.fetchPromise
 		} else {
@@ -24,8 +24,53 @@ export class AsyncCache<T> {
 
 	async get() {
 		if (this.isOutOfDate) {
-			await this._fetch()
+			await this.fetch()
 		}
 		return this.data
+	}
+}
+
+export class AsyncDictCache<T extends object, K extends keyof T> {
+	private lastCacheTime: number | undefined = undefined
+	private data: Map<T[K], T> = new Map()
+	private fetchPromise: Promise<void> | undefined = undefined
+
+	constructor(private updater: () => Promise<T[]>, private key: K, private cacheTime = 15) {}
+
+	async fetch() {
+		try {
+			const result = await this.updater()
+			this.data.clear()
+			for (const obj of result) {
+				this.data.set(obj[this.key], obj)
+			}
+			this.lastCacheTime = Date.now()
+		} catch (err) {}
+	}
+
+	async doFetch() {
+		if (this.fetchPromise) await this.fetchPromise
+		else {
+			this.fetchPromise = this.fetch()
+			await this.fetchPromise
+		}
+	}
+
+	get isOutOfDate() {
+		return !this.data || Date.now() - (this.lastCacheTime ?? 0) > this.cacheTime * 1000
+	}
+
+	async get(key: T[K]) {
+		if (this.isOutOfDate || !this.data.has(key)) {
+			await this.doFetch()
+		}
+		return this.data.get(key)
+	}
+
+	async values() {
+		if (this.isOutOfDate) {
+			await this.doFetch()
+		}
+		return [...this.data.values()]
 	}
 }
