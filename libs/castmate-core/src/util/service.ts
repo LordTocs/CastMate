4b3d4b502b00db6b5ddd1811d9bc71383af66e6a
LordTocs <lordtocs@gmail.com>
2026-09-14 20:46:44 -0400

    Add Experimental Default Service that gets lazy inited
    Typescript is not happy with this one due to what looks like a language design mistake

--- libs/castmate-core/src/util/service.ts
+++ libs/castmate-core/src/util/service.ts
@@ -17,4 +17,17 @@ export function Service<ConstructorType extends new (...args: any[]) => any>(con
 	}
 }
 
+export function DefaultService<ConstructorType extends new () => any>(constructor: ConstructorType) {
+	//@ts-ignore - This is a typescript compiler bug, you can actually restrict constructor types on mixins and is a basic feature of a language. This kind of oversight from the language team is growing old.
+	return class DefaultService extends constructor {
+		private static _instance: InstanceType<ConstructorType>
+
+		static getInstance(): InstanceType<ConstructorType> {
+			if (!this._instance) {
+				this._instance = new constructor()
+			}
+			return this._instance
+		}
+	}
+}
 ///
